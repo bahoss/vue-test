b00ba71d93e0ca871d8453676cdd154bfc9ec145
Баходир Кадыров <bakhodir.kadyrov@hawex.io>
2023-11-04 10:59:02 +0600

    add v-if

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,16 @@
 <template>
-     <div>
-        <h3>User list</h3>
+    <div v-if="posts.length > 0">
+        <div>
+            <h3>User list</h3>
+        </div>
+        <post-item  
+            v-for="post in posts"
+            :post="post"
+            :key="post.id"
+            @remove="$emit('remove', post)"
+        />
     </div>
-    <post-item  
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    />
+    <h2 v-else style="color: red">List is empty</h2>
 </template>
 
 <script>
