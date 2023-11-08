73c6dd2e5b65303872830e053b5bafcc9c5504ab
Баходир Кадыров <bakhodir.kadyrov@hawex.io>
2023-11-08 20:31:08 +0600

    add animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -3,12 +3,15 @@
         <div>
             <h3>User list</h3>
         </div>
-        <post-item  
-            v-for="post in posts"
-            :post="post"
-            :key="post.id"
-            @remove="$emit('remove', post)"
-        />
+        <transition-group name="post-list">
+           <post-item  
+                v-for="post in posts"
+                :post="post"
+                :key="post.id"
+                @remove="$emit('remove', post)"
+            />  
+        </transition-group>
+       
     </div>
     <h2 v-else style="color: red">List is empty</h2>
 </template>
@@ -29,6 +32,21 @@ export default {
 </script>
 
 <style scoped>
-
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: all 0.4s ease;
+}
 
 </style>
